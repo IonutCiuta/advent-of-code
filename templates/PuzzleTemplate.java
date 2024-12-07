b4001e0f24cc8c745710b4ebf9dda6c1b33060e5
Ionuț Ciuta <ionutciuta24@gmail.com>
2024-12-07 18:51:08 +0200

    Simplify src/main template

--- templates/PuzzleTemplate.java
+++ templates/PuzzleTemplate.java
@@ -1,33 +1,18 @@
 package com.ionutciuta.puzzles._#YEAR#;
 
 import com.ionutciuta.puzzles.Puzzle;
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
 
 public class Puzzle__DAY__ extends Puzzle<Integer> {
 
     @Override
     public Integer solvePart1(String inputFile) {
-        final var file = new File(inputFile);
-        var result = -1;
-        try (var it = FileUtils.lineIterator(file)) {
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        var r = 0;
+        return r;
     }
 
     @Override
     protected Integer solvePart2(String inputFile) {
-        final var file = new File(inputFile);
-        var result = -1;
-        try (var it = FileUtils.lineIterator(file)) {
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        var r = 0;
+        return r;
     }
 }
